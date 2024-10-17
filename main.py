11bdd118ff93ee1fe4f1626b569ccd850271ee6a
Osinachi Okpara <osinachi.okpara@eng.uniben.edu>
2024-10-17 08:42:06 +0100

    - added task_type and event name variables - added UPDATE_1 to the docstring - added comments for pseudocode

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 '''
 Hello World
 
-I want to create a calendar event/task creating bot that has reminder presets. I usually set my reminders with 
+I want to create a calendar event/task creating bot that has reminder presets. 
+I usually set my reminders with about 5 different reminders: 0min, 30min, 1hr, 3hr & 5hr
+It can get repetitive doing this
+So I decided to create a Python GUI to help me achieve this.
+The bot will take only a name... 
+and you'll be able to choose how critical the event is so you can set how many reminders you want
+let's see how it goes
 
+UPDATE:
+Just realised I'll need to add date select too
 '''
 
+# Gonna need to learn how to use API's
+# Get the Google Calendar API
+
+task_type = ""
+event_name = input("Name of Event")
+
+j 
